6c1901c0c7df52fe3f90dca5095057b9a44ada75
William Moehlig <william.moehlig@hotmail.com>
2019-09-18 10:25:29 +0200

    Changed to IIFE style

--- calc.js
+++ calc.js
@@ -1,6 +1,11 @@
-const calc = {
-    sum(value1, value2){
+var calc = (function(){
+    
+    function sum(value1, value2){
         //parseInt always needs 10 behind the value to avoid issues
         return parseInt(value1,10) + Number(value2);
     }
-}
+    
+    return{
+        sum
+    };
+})();
