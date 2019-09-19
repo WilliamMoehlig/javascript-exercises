11d0620ed9c292bd2767da1b54242faacce0aa40
William Moehlig <william.moehlig@hotmail.com>
2019-09-19 09:20:47 +0200

    Multiple solutions for second prototype exercise

--- ex 6 - prototypes/animal.js
+++ ex 6 - prototypes/animal.js
@@ -1,16 +1,82 @@
-class Animal {
-  eat = function() {
-    console.log("I eat");
-  };
+//first solution
+console.log("First solution");
 
-  constructor() {}
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+  eat() {
+    console.log("I eat first");
+  }
 }
 
-const rabbit = Object.create(Animal);
-rabbit.jump = function() {
-  console.log("I jump");
+class Rabbit extends Animal {
+  jump() {
+    console.log("I jump first");
+  }
+}
+
+const rabbit = new Rabbit("rabbit");
+
+rabbit.eat();
+rabbit.jump();
+console.log(rabbit.name);
+console.log("");
+
+//second solution
+console.log("Second solution");
+
+const animal2 = Object.create(null);
+animal2.eat = function() {
+  console.log("I eat second");
 };
 
-const animal = Object.create(Animal);
+const rabbit2 = Object.create(animal2);
+rabbit2.jump = function() {
+  console.log("I jump second");
+};
 
-console.log(rabbit.jump());
+rabbit2.eat();
+rabbit2.jump();
+console.log("");
+
+//third solution
+console.log("Third solution");
+
+function Animal3() {}
+
+Animal3.prototype.eat = function() {
+  console.log("I eat third");
+};
+
+const rabbit3 = new Animal3();
+rabbit3.jump = function() {
+  console.log("I jump third");
+};
+
+rabbit3.eat();
+rabbit3.jump();
+console.log("");
+
+//fourth solution
+console.log("Fourth solution");
+
+function Animal4() {}
+
+Animal4.prototype.eat = function() {
+  console.log("I eat fourth");
+};
+
+function Rabbit4() {}
+
+Rabbit4.prototype.__proto__ = Animal4.prototype;
+Rabbit4.prototype.constructor = Animal4;
+
+Rabbit4.prototype.jump = function() {
+  console.log("I jump fourth");
+};
+
+const rabbit4 = new Rabbit4();
+
+rabbit4.eat();
+rabbit4.jump();
